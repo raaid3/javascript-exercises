15468e64ba94a14dceafb8635cf37e8c687b04d0
Dhyan Rai <172551908+raaid3@users.noreply.github.com>
2024-11-28 01:31:53 -0500

    Complete 01 and 02 exercises
    Play around with testing mechanism and touch up on js basics

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if(num < 0){
+        return "ERROR";
+    }
+    else {
+        let curStr = "";
+        for(let i = 0; i < num; i++){
+            curStr = curStr + str;
+        }
+        return curStr;
+    }
 };
 
 // Do not edit below this line
