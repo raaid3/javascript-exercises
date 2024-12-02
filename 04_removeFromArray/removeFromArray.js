66d7cace8ca8d5f5d50f26bf21e402fff0267d61
Dhyan Rai <172551908+raaid3@users.noreply.github.com>
2024-12-02 00:30:17 -0500

    Complete exercises 03-07
    Use basic loops and math operators to complete simple tasks

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr1, ...arr2) {
+    return arr1.filter(x => !arr2.includes(x));
 };
+
+// the above is a variadic function
+// it has indefinite arity (number of arguments)
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if(!Number.isInteger(a) || !Number.isInteger(b)) {
+        return 'ERROR';
+    }
+    else if(a <= 0 || b <= 0){
+        return 'ERROR';
+    }
+    else {
+        let greaterInt = Math.max(a, b);
+        let smallerInt = Math.min(a, b);
+        let acc = 0;
 
+        for(let i = smallerInt; i <= greaterInt; i++) {
+            acc += i;
+        }
+        return acc;
+    }
 };
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
